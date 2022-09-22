86e3bfce1fb61289781c8ec87fcdf0beb9610bff
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-09-22 00:34:43 -0300

    chore: initial version

--- projeto_otimizacao_algoritmos/t2/Main.java
+++ projeto_otimizacao_algoritmos/t2/Main.java
@@ -8,14 +8,46 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-public class Main {
+class Main {
+  public static int negInf = -1000000;
+
   public static void main(String[] args) {
-    Main main = new Main();
-    Map m = new Map(args[0]);
-    ArrayList<ArrayList<String>> map = m.getMap();
+    Main main;
+    Map m;
+    ArrayList<ArrayList<String>> map;
+    final int SIZE, POS_INIT_X, POS_INIT_Y;
+
+    main = new Main();
+    m = new Map(args[0]);
+    map = m.getMap();
+
+    SIZE = map.size();
+    POS_INIT_X = 0;
+    POS_INIT_Y = SIZE - 1;
+    int golds = main.giveMeGold(POS_INIT_X, POS_INIT_Y, map, SIZE, Main.negInf, 0, 0);
+    System.out.println("golds" + golds);
+
   }
 
-  public void someRecursion() {
-    System.out.println("some cool algorithm");
+  int giveMeGold(int x, int y, ArrayList<ArrayList<String>> map, int size, int best, int bag, int res) {
+    if (x < 0 || x > size)
+      return negInf;
+    if (y < 0 || y > size)
+      return negInf;
+    if (map.get(x).get(y) == "x")
+      return negInf;
+
+    int gold = Integer.parseInt(map.get(x).get(y));
+
+    System.out.println(gold);
+    res = giveMeGold(x, y - 1, map, size, best, bag, res);
+    if (res < best) {
+      best = res;
+      bag += best;
+    } else {
+      best = giveMeGold(x + 1, y, map, size, best, bag, res);
+      bag += best;
+    }
+    return bag;
   }
 }
