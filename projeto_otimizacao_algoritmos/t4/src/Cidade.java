41314ced4022f0ebbd82f4a1e04e19e7a57de070
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-11-07 19:27:13 -0300

    alguma coisa

---
+++ projeto_otimizacao_algoritmos/t4/src/Cidade.java
@@ -0,0 +1,62 @@
+import java.util.HashMap;
+
+public class Cidade implements Comparable<Cidade> {
+    private double longitude;
+    private double latitude;
+    private String codCidade;
+    private int id;
+    private HashMap<Integer, Double> mapaDistancia;
+
+
+    public Cidade(double longitude, double latitude, String codCidade) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.codCidade = codCidade;
+        String aux = "";
+
+        for (int i = 0; i < codCidade.length(); i++) {
+            char aChar = codCidade.charAt(i);
+            if (Character.isDigit(aChar)) {
+                aux += aChar;
+            }
+        }
+
+        this.id = Integer.parseInt(aux);
+    }    
+
+    public void setMapaDistancia(HashMap<Integer, Double> mapa) {
+        this.mapaDistancia = mapa;
+    }
+
+    public HashMap<Integer, Double> getMapaDistancia() {
+        return this.mapaDistancia;
+    }
+
+    public String toString() {
+        return "codCidade=" + codCidade + ";latidude=" + this.latitude + ";longitude=" + this.longitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCodCidade() {
+        return codCidade;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public int compareTo(Cidade outraCidade) {
+        if (this.id < outraCidade.id)
+            return -1;
+        if (this.id > outraCidade.id)
+            return 1;
+        return 0;
+    }
+}
--- projeto_otimizacao_algoritmos/t4/src/RomariaDaVovo.java
+++ projeto_otimizacao_algoritmos/t4/src/RomariaDaVovo.java
@@ -1,62 +1,101 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Random;
 import java.util.Scanner;
 
+import javax.naming.LimitExceededException;
+
 public class RomariaDaVovo {
-	private final int TAM = 2009;
-	private String[][] populacao;
+	private int TAM;
+	private int TAM_CROMOSSOMOS = 30; //quantidade de cromossomos por geracao
+	private ArrayList<Cidade> mapaCidades = new ArrayList<>(TAM);
+	private ArrayList<Double> resultadosHeuristica = new ArrayList<>(TAM);
+	private ArrayList<ArrayList<Integer>> populacao = new ArrayList<>(TAM);
 
 	public static void main(String args[]) {
 		new RomariaDaVovo().run();
 	}
 
-	private void run() {
-		leArquivo();
-
-		int melhor;
-		for (int g = 0; g < 300; g++) {
-			calculaHeuristica();
-			printPopulacao();
-		}
-	}
-
-	private void printPopulacao() {
-		String msg;
-		for (int i = 0; i < TAM; i++) {
-			msg = "";
-			for(int j = 0; j < 3; j++)
-				msg += populacao[i][j] + " ";
-			System.out.println(msg);
-		}
-	}
-
-	private void leArquivo() {
-	    File myObj = new File("data.txt");
+	public void preProcessamento() {
+	    File myObj = new File("data1.txt");
 	    try {
 	      Scanner myReader = new Scanner(myObj);
-	      myReader.nextLine(); // nao preciso do tamanho da populacao, ja coloquei la em cima
-	      populacao = new String[TAM][3];
-
+	      TAM = Integer.parseInt(myReader.nextLine()); // nao preciso do tamanho da populacao, ja coloquei la em cima
 	      for (int i = 0; i < TAM; i++) {
 			String[] line = myReader.nextLine().split(" ");
-			for (int j = 0; j < 3; j++) {
-				populacao[i][j] = line[j];
-			}
+			mapaCidades.add(new Cidade(Double.parseDouble(line[0]), Double.parseDouble(line[1]), line[2]));
 	      }
 	      myReader.close();
 	    } catch (Exception e) {
 		    System.out.println("ops, erro ao ler o arquivo");
 	    }
+
+		Collections.sort(mapaCidades);
+
+		// for (int i = 0; i < mapaCidades.size(); i++) {
+		// 	Cidade sentinela = mapaCidades.get(i);
+		// 	HashMap<Integer, Double> mapaDistancias = new HashMap<>();
+		// 	for ( Cidade c : mapaCidades ) {
+		// 		double distancia = distanciaEuclidiana(
+		// 			sentinela.getLatitude(), 
+		// 			sentinela.getLongitude(), 
+		// 			c.getLatitude(), 
+		// 			c.getLongitude()
+		// 		);
+		// 		mapaDistancias.put(c.getId(), distancia);
+		// 	}
+		// 	sentinela.setMapaDistancia(mapaDistancias);
+		// }
+	}
+
+	private void run() {
+		preProcessamento();
+		init();
+		printPopulacao();
+	}
+
+	void calculaHeuristica() {
+		resultadosHeuristica = new ArrayList<>();
+		for(int i = 0; i < TAM_CROMOSSOMOS; i++){
+			resultadosHeuristica.add(heuristica(i));
+		  }
+	}
+
+	double heuristica(int individuo) {
+		return -1.0;
+	}
+
+	public void init() {
+		Random rand = new Random();
+		populacao = new ArrayList<>();
+
+		for (int i=0; i < TAM_CROMOSSOMOS; i++) {
+			ArrayList<Integer> rota = new ArrayList<>();
+			for (int j=0; j < mapaCidades.size(); j++) {
+				int cidadeId = 1 + rand.nextInt(TAM);
+				while (rota.contains(cidadeId)) cidadeId = 1 + rand.nextInt(TAM);
+				rota.add(cidadeId);
+			}
+			populacao.add(rota);
+		}
+
+		System.out.println(populacao.get(0).toString());
+	}
+
+	private void printPopulacao() {
+		populacao.forEach(System.out::println);
 	}
 
 	private double distanciaEuclidiana(double x1, double x2, double y1, double y2) {
 		return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 	}
 
-	void init() {}
-
 	void heuristica() {}
 
-	void calculaHeuristica() {}
+	
 
 	// elitismo
 	void pegaMelhor() {} 
